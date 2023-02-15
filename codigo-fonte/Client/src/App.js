d8699181f47484223e34aa013461786bebafcda3
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-15 17:30:12 -0300

    Delete old frontend ON BRANCH

--- codigo-fonte/Client/src/App.js
+++ codigo-fonte/Client/src/App.js
@@ -1,35 +1,24 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./components/pages/Home";
-import Imobiliarias from "./components/pages/Imobiliarias";
-import Header from "./components/layout/Header";
-import SideBar from "./components/layout/SideBar";
-import {toast, ToastContainer} from "react-toastify"
-import "react-toastify/dist/ReactToastify.css";
-
-import Container from "./components/layout/Container";
-import NovaImobiliaria from "./components/pages/NovaImobiliaria";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-
-
-
   return (
-
-    <Router>
-    <Header />
-    <SideBar /> 
-    <Container>
-      <Routes>
-        <Route path="/" element= {<Home />} />
-        <Route path="/imobiliarias" element= {<Imobiliarias />} />
-        <Route path="/imobiliarias/novaImobiliaria" element= {<NovaImobiliaria />} />
-        <Route path="/imobiliarias/detalhesImobiliaria/:id" element= {<NovaImobiliaria detalhes={true}/>} />
-      </Routes> 
-    </Container>
-    <ToastContainer autoClose={3000} position={toast.POSITION.TOP_CENTER}/>
-    </Router> 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 

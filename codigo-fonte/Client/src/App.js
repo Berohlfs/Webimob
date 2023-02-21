08395e6a1b4ffef4c5ef10277ebf128fc5c7bfa6
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-20 21:10:56 -0300

    Creating new Header (.js and .css)

--- codigo-fonte/Client/src/App.js
+++ codigo-fonte/Client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './layouts/Header'
 
 function App() {
   return (
     <>
+    <Header/>
     </>
   )
 }
---
+++ codigo-fonte/Client/src/layouts/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+import webimob_logo from '../images/logo.png'
+const Header = ()=> {
+    return (
+        <header>
+            <img src={webimob_logo} alt={'Logo Webimob'}/>
+        </header>
+    )
+}
+
+export default Header

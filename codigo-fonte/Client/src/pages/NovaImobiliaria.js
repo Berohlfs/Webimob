180446d96f8a88a228d910248994a613561e65fb
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-23 00:49:03 -0300

    create test form

--- codigo-fonte/Client/src/pages/NovaImobiliaria.js
+++ codigo-fonte/Client/src/pages/NovaImobiliaria.js
@@ -6,15 +6,26 @@ import InputMasked from '../assets/InputMasked'
 import InputDefault from '../assets/InputDefault'
 
 const NovaImobiliaria = (props)=> {
+    const testForm = (e)=> {
+      e.preventDefault()
+      console.log(e.target[0].value, e.target[1].value, e.target[2].value )
+    }
+
     return (
         <>
           <Header title={'Nova imobiliária'}/>
           <Menu responsiveFunction={props.responsiveFunction} responsiveState={props.responsiveState} />
           <Container responsiveState={props.responsiveState}>
+            <form onSubmit={testForm}>
 
-            <InputMasked label={'CPF'} size={123} input_name={'cpf'} input_mask={'999.999.999-99'}/>
-            <InputMasked label={'CPF'} size={123} input_name={'cpf'} input_mask={'999.999.999-99'}/>
-            <InputDefault label={'Razão Social'} size={300} input_name={'Razão Social'}/>
+              <InputDefault label={'Razão Social'} size={300} input_name={'Razão Social'}/>
+              <InputMasked label={'CPF'} size={123} input_name={'cpf'} input_mask={'999.999.999-99'}/>
+              <InputMasked label={'CPF'} size={123} input_name={'cpf'} input_mask={'999.999.999-99'}/>
+
+              <button type={'submit'} >Enviar</button>
+
+            </form>
+
 
           </Container>
         </>

154be1a6f35563f394f95db8508ae28a891fd664
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-23 22:32:56 -0300

    create H1 div

--- codigo-fonte/Client/src/pages/NovaImobiliaria.js
+++ codigo-fonte/Client/src/pages/NovaImobiliaria.js
@@ -5,11 +5,18 @@ const NovaImobiliaria = ()=> {
 
     return (
         <>
+          <div id={'page-info-div'}>
+            <h1>Nova imobiliária</h1>
+          </div>
 
           {/* InputDefault props -> label, input_length, input_type, input_width, input_name, input_mask*/}
+          <form>
 
-          <InputDefault label={'Razão social'} input_width={'300px'} input_name={'nome'}/>
-          <InputDefault label={'Número'} input_length={''} input_width={'200px'} input_name={'numero'} input_mask={''}/>
+            <InputDefault label={'Razão social'} input_width={'300px'} input_name={'nome'}/>
+            <InputDefault label={'Número'} input_length={''} input_width={'200px'} input_name={'numero'} input_mask={''}/>
+            
+          </form>
+
 
         </>
       )
--- codigo-fonte/Client/src/pages/PaginaInicial.js
+++ codigo-fonte/Client/src/pages/PaginaInicial.js
@@ -3,7 +3,9 @@ const PaginaInicial = ()=> {
 
     return (
         <>
-
+          <div id={'page-info-div'}>
+            <h1>Página inicial</h1>
+          </div>
         </>
       )
 }

13ca3b752f6242c6acd204b06bebff160ba25468
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-24 19:10:38 -0300

    Fixing optative toggle func problem

--- codigo-fonte/Client/src/assets/Checkbox.js
+++ codigo-fonte/Client/src/assets/Checkbox.js
@@ -3,7 +3,7 @@ import './Checkbox.css'
 const Checkbox = ({toggle_func='', label='', input_name=''}) => {
     return (
         <div className={'checkbox-div'}>
-            <input onInput={toggle_func} id={input_name} name={input_name} type={'checkbox'}/>
+            <input onInput={toggle_func == '' ? null : toggle_func} id={input_name} name={input_name} type={'checkbox'}/>
             <label htmlFor={input_name}>{label}</label>
         </div>
     )

6312319ce277dbed1a3ffdf6f7d7ba35047fc242
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-02-28 18:53:16 -0300

    components line spacing

--- codigo-fonte/Client/src/assets/ButtonDefault.js
+++ codigo-fonte/Client/src/assets/ButtonDefault.js
@@ -3,12 +3,14 @@ import './ButtonDefault.css'
 
 const ButtonDefault = ({button_style=true, img_src='', label='', clickFunc=null, button_form='', button_type='submit'})=> {
     return (
+
             <button type={button_type} form={button_form} onClick={clickFunc} className={`button-default ${button_style===true ? 'button-default-positive' : button_style===false ? 'button-default-negative' : 'button-default-icon'}`}>
 
                 {label}
                 <img className={`${!img_src && 'img-display-none'}`} src={img_src} alt={'Detalhes'}/>
 
             </button>
+            
     )
 }
 
